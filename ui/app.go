3052af374433455ca2d4e12290716a98f2b40dd2
lee <lijie@dragonest.com>
2024-08-15 09:20:50 +0800

    新增初始化参数 http.FileSystem

--- ui/app.go
+++ ui/app.go
@@ -1,133 +1,139 @@
 package ui
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"os/signal"
-	"path/filepath"
-	"strings"
-	"time"
+    "fmt"
+    "log"
+    `net/http`
+    "os"
+    "os/signal"
+    "path/filepath"
+    "strings"
+    "time"
 
-	"github.com/gamelee/robot/invoker"
-	"github.com/gamelee/robot/ui/lorca"
+    "github.com/gamelee/robot/invoker"
+    "github.com/gamelee/robot/ui/lorca"
 )
 
 type WebApp struct {
-	*Config
-	*invoker.FuncManager
-	ui   lorca.UI
-	stop <-chan string
-	fs   *FileServer
+    *Config
+    *invoker.FuncManager
+    ui   lorca.UI
+    stop <-chan string
+    fs   *FileServer
 }
 
 func NewApp(opts ...Option) *WebApp {
-	cfg := new(Config)
-	for i := range opts {
-		err := opts[i].Apply(cfg)
-		if err != nil {
-			log.Panicln(err)
-		}
-	}
-	this := &WebApp{
-		Config:      cfg,
-		FuncManager: invoker.NewFuncManager(),
-		fs:          NewFileServer(http.Dir(cfg.AssetPath), cfg.Port),
-	}
-	this.init()
-	this.stop = make(chan string)
-	return this
+    cfg := new(Config)
+    for i := range opts {
+        err := opts[i].Apply(cfg)
+        if err != nil {
+            log.Panicln(err)
+        }
+    }
+    this := &WebApp{
+        Config:      cfg,
+        FuncManager: invoker.NewFuncManager(),
+    }
+
+    if cfg.FS != nil {
+        this.fs = NewFileServer(cfg.FS, cfg.Port)
+    } else {
+        this.fs = NewFileServer(http.Dir(cfg.AssetPath), cfg.Port)
+    }
+
+    this.init()
+    this.stop = make(chan string)
+    return this
 }
 
 func (wa *WebApp) init() {
 
-	args := make([]string, 0)
-	log.Printf("配置信息: %#v", wa.Config)
-	args = append(args, "--remote-allow-origins=*")
-	args = append(args, "--disable-automation")
-	if len(wa.Config.Extension) != 0 {
-		args = append(args, "--load-extension="+strings.Join(wa.Config.Extension, ","))
-	}
-	go wa.fs.Run()
+    args := make([]string, 0)
+    log.Printf("配置信息: %#v", wa.Config)
+    args = append(args, "--remote-allow-origins=*")
+    args = append(args, "--disable-automation")
+    if len(wa.Config.Extension) != 0 {
+        args = append(args, "--load-extension="+strings.Join(wa.Config.Extension, ","))
+    }
+    go wa.fs.Run()
 
-	var err error
-	if wa.CachePath != "" {
-		wa.CachePath, err = filepath.Abs(wa.CachePath)
-		if err != nil {
-			log.Fatal("init cache path", err.Error())
-		}
-	}
-	wa.ui, err = lorca.New("", wa.CachePath, wa.Width, wa.Height, args...)
-	if err != nil {
-		log.Fatal("init ui", err.Error())
-	}
+    var err error
+    if wa.CachePath != "" {
+        wa.CachePath, err = filepath.Abs(wa.CachePath)
+        if err != nil {
+            log.Fatal("init cache path", err.Error())
+        }
+    }
+    wa.ui, err = lorca.New("", wa.CachePath, wa.Width, wa.Height, args...)
+    if err != nil {
+        log.Fatal("init ui", err.Error())
+    }
 }
 
 func (wa *WebApp) Run() (err error) {
-	defer func() {
-		if err != nil {
-			log.Fatalf("运行出错:%s", err.Error())
-		}
-		if buf := recover(); buf != nil {
-			err = fmt.Errorf("程序中☞：%#v", buf)
-		}
-		if wa.ui != nil {
-			wa.ui.Close()
-		}
-	}()
-	time.Sleep(time.Second)
-	if err = wa.ui.Bind("GO", wa.invoke); err != nil {
-		return err
-	}
-	wa.ui.Load(wa.fs.Addr())
-	defer func() { _ = wa.ui.Close() }()
-	c := make(chan os.Signal)
-	signal.Notify(c, os.Interrupt)
-	reason := "unknown"
-	select {
-	case <-c:
-		reason = "system"
-	case <-wa.ui.Done():
-		reason = "ui"
-	case reason = <-wa.stop:
-	}
-	log.Println("程序退出，原因：", reason)
-	return nil
+    defer func() {
+        if err != nil {
+            log.Fatalf("运行出错:%s", err.Error())
+        }
+        if buf := recover(); buf != nil {
+            err = fmt.Errorf("程序中☞：%#v", buf)
+        }
+        if wa.ui != nil {
+            wa.ui.Close()
+        }
+    }()
+    time.Sleep(time.Second)
+    if err = wa.ui.Bind("GO", wa.invoke); err != nil {
+        return err
+    }
+    wa.ui.Load(wa.fs.Addr())
+    defer func() { _ = wa.ui.Close() }()
+    c := make(chan os.Signal)
+    signal.Notify(c, os.Interrupt)
+    reason := "unknown"
+    select {
+    case <-c:
+        reason = "system"
+    case <-wa.ui.Done():
+        reason = "ui"
+    case reason = <-wa.stop:
+    }
+    log.Println("程序退出，原因：", reason)
+    return nil
 }
 
 func (wa *WebApp) invoke(call *invoker.Call) *invoker.CallRst {
-	log.Println("jscall", call)
-	rst := wa.Call(call)
-	if tmp, ok := rst.Data.([]interface{}); ok {
-		if len(tmp) == 2 {
-			rst.Data = tmp[0]
-			err, ok := tmp[1].(error)
-			if ok && err != nil {
-				rst.Code = invoker.CodeFailed
-				rst.Message = err.Error()
-			}
-		}
-	}
-	return rst
+    log.Println("jscall", call)
+    rst := wa.Call(call)
+    if tmp, ok := rst.Data.([]interface{}); ok {
+        if len(tmp) == 2 {
+            rst.Data = tmp[0]
+            err, ok := tmp[1].(error)
+            if ok && err != nil {
+                rst.Code = invoker.CodeFailed
+                rst.Message = err.Error()
+            }
+        }
+    }
+    return rst
 }
 
 func (wa *WebApp) CallJS(call *invoker.Call) *invoker.CallRst {
-	rst := new(invoker.CallRst)
-	val := wa.ui.Eval(fmt.Sprintf("CallJS(%s)", call))
-	if val.Err() != nil {
-		rst.Code = invoker.CodeFailed
-		rst.Message = fmt.Sprintf("CallJS failed:%w", val.Err().Error())
-		return rst
-	}
-	err := val.To(rst)
-	if err != nil {
-		rst.Code = invoker.CodeFailed
-		rst.Message = fmt.Sprintf("Parse Result failed:%s", err.Error())
-	}
-	return rst
+    rst := new(invoker.CallRst)
+    val := wa.ui.Eval(fmt.Sprintf("CallJS(%s)", call))
+    if val.Err() != nil {
+        rst.Code = invoker.CodeFailed
+        rst.Message = fmt.Sprintf("CallJS failed:%w", val.Err().Error())
+        return rst
+    }
+    err := val.To(rst)
+    if err != nil {
+        rst.Code = invoker.CodeFailed
+        rst.Message = fmt.Sprintf("Parse Result failed:%s", err.Error())
+    }
+    return rst
 }
 
 func (wa *WebApp) RunJS(code string) lorca.Value {
-	return wa.ui.Eval(code)
+    return wa.ui.Eval(code)
 }
--- ui/config.go
+++ ui/config.go
@@ -1,66 +1,71 @@
 package ui
 
+import (
+    `net/http`
+)
+
 type Config struct {
-	Width     int
-	Height    int
-	CachePath string
-	Extension []string
-	AssetPath string
-	Name      string
-	Args      []string
-	Port      int
+    Width     int
+    Height    int
+    CachePath string
+    Extension []string
+    AssetPath string
+    Name      string
+    Args      []string
+    Port      int
+    FS        http.FileSystem
 }
 
 type Option interface {
-	Apply(config *Config) error
+    Apply(config *Config) error
 }
 
 type WithName string
 
 func (a WithName) Apply(config *Config) error {
-	config.Name = string(a)
-	return nil
+    config.Name = string(a)
+    return nil
 }
 
 type WithSize [2]int
 
 func (s WithSize) Apply(config *Config) error {
-	config.Width = s[0]
-	config.Height = s[1]
-	return nil
+    config.Width = s[0]
+    config.Height = s[1]
+    return nil
 }
 
 type WithCachePath string
 
 func (l WithCachePath) Apply(config *Config) error {
-	config.CachePath = string(l)
-	return nil
+    config.CachePath = string(l)
+    return nil
 }
 
 type WithExtensions []string
 
 func (l WithExtensions) Apply(config *Config) error {
-	config.Extension = l
-	return nil
+    config.Extension = l
+    return nil
 }
 
 type WithAssetPath string
 
 func (l WithAssetPath) Apply(config *Config) error {
-	config.AssetPath = string(l)
-	return nil
+    config.AssetPath = string(l)
+    return nil
 }
 
 type WithArgs []string
 
 func (a WithArgs) Apply(config *Config) error {
-	config.Args = append(config.Args, a...)
-	return nil
+    config.Args = append(config.Args, a...)
+    return nil
 }
 
 type WithPort int
 
 func (p WithPort) Apply(config *Config) error {
-	config.Port = int(p)
-	return nil
+    config.Port = int(p)
+    return nil
 }
